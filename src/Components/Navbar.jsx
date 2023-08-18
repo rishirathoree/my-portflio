1bf2e29fa083f43d7c0cb74402724c50fa37b8c8
rishirathoree <rishirathoree3@gmail.com>
2023-08-18 15:16:56 +0530

    new layout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = ({showProjects,setShowProjects}) => {
     <>
     <div className='p-10 fixed w-full top-0 right-0 flex items-center justify-between'>
       <p className='font-bold leading-3 -tracking-[2px] text-2xl text-[#6ace60]'>Rishi</p>
-      <div onClick={toggleProject} className='space-y-1 items-center flex justify-center flex-col'>
-        <div className='block w-2 h-[1px] bg-white'></div>
+      <div onClick={toggleProject} className={`space-y-1 items-center flex justify-center flex-col`}>
+        <div className={`block w-2 h-[1px] bg-white`}></div>
         <div className='block w-4 h-[1px] bg-white'></div>
-        <div className='block w-2 h-[1px] bg-white'></div>
+        <div className={`block w-2 h-[1px] bg-white`}></div>
       </div>
     </div>
     </>
